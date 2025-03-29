b31533f312ef4f26485875e9c327bfd4b7ed3191
Kidus-Sofonias <sofoniaskidus@gmail.com>
2025-03-29 19:25:54 +0300

    fix case for deploy middleWare

--- backend/routes/answerRoute.js
+++ backend/routes/answerRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const authMiddleware = require("../middleware/authMiddleware");
+const authMiddleware = require("../middleWare/authMiddleware");
 const {createAnswer, GetAnswersByQuestionId}  = require("../controller/answer")
 
 router.post("/answers/create", authMiddleware, createAnswer); 
